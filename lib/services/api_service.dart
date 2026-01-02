e7905485dea5bb93d394c6fbd4a30f0c1757e0a1
alyayes <fajrialyaya78@gmail.com>
2026-01-02 13:14:19 +0700

    api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -410,20 +410,3 @@ class ApiService {
     }
   }
 } 
-import '../models/post_model.dart';
-
-class ApiService {
-  // GANTI IP INI dengan IP Laptop Anda (hasil ipconfig)
-  static const String baseUrl = "http://30.10.36.96:8000/api";
-
-  Future<List<PostModel>> fetchTrends() async {
-    final response = await http.get(Uri.parse('$baseUrl/trends'));
-
-    if (response.statusCode == 200) {
-      List data = json.decode(response.body)['data'];
-      return data.map((json) => PostModel.fromJson(json)).toList();
-    } else {
-      throw Exception('Gagal memuat data dari server');
-    }
-  }
-}
